9e09a0a73205ff8cd7ec42bdc5f020fbf0694ac5
blessedsoy <unmigong@gmail.com>
2016-10-04 13:26:09 -0400

    commented out all the unneccessary funtions

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,28 +1,28 @@
-class MemosController < ApplicationController
+# class MemosController < ApplicationController
   
-  def create
-    @memo = Memo.create(memo_params)
-      redirect_to lists_path
-  end
+#   def create
+#     @memo = Memo.create(memo_params)
+#       redirect_to lists_path
+#   end
 
-  def update
-    @memo = Memo.find_by(id: params[:memo][:id], list_id: params[:memo][:list_id])
-    @memo.update(memo_params)
-    redirect_to lists_path
-  end
+#   def update
+#     @memo = Memo.find_by(id: params[:memo][:id], list_id: params[:memo][:list_id])
+#     @memo.update(memo_params)
+#     redirect_to lists_path
+#   end
 
-  def destroy
-    @memo = Memo.find_by(id: params[:id])
-    list = List.find_by(id: @memo.list_id)
-    list.memo.destroy
-    @memo.destroy
-    redirect_to lists_path
-  end
+#   def destroy
+#     @memo = Memo.find_by(id: params[:id])
+#     list = List.find_by(id: @memo.list_id)
+#     list.memo.destroy
+#     @memo.destroy
+#     redirect_to lists_path
+#   end
 
-  private
+#   private
 
-    def memo_params
-      params.require(:memo).permit(:id, :content, :list_id)
-    end
+#     def memo_params
+#       params.require(:memo).permit(:id, :content, :list_id)
+#     end
 
-end
+# end
