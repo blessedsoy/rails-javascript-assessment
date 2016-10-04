7708a570c63ed8e9829b593e305837277b1f58e2
blessedsoy <unmigong@gmail.com>
2016-10-04 13:35:00 -0400

    change like routes and added delete_memo and the_user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # resources :votes
-  resources :memos
+  # resources :memos
   resources :lists
   resources :sample_sales
   root to: 'home#index'
@@ -8,10 +8,13 @@ Rails.application.routes.draw do
   resources :lists, only: [:index] do
     resources :sample_sales, only: [:show]
   end
+  
   post "/add_to_list" => 'lists#create'
-  get "/like" => 'sample_sales#like'
-  get "/dislike" => 'sample_sales#dislike'
+  get "/like" => 'votes#like'
+  get "/dislike" => 'votes#dislike'
   get "/sort_by_likes" => 'sample_sales#sort_by_likes'
   get "/sort_by_dates" => 'lists#sort_by_dates'
-  
+  post "/delete_memo/:id" => 'lists#delete_memo', as: 'delete_memo'
+  get '/the_user' => 'votes#the_user'
+
 end
