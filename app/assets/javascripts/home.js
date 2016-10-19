8361493151bba540252ce73496d5e889707d5e6c
blessedsoy <unmigong@gmail.com>
2016-10-18 20:47:02 -0400

    trying to change animation effect

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,19 +2,19 @@
 
 $(function() {
 
-  $('.table-box').addClass('animated fadeInDown');
-  $('#summerSample').addClass('animated fadeInDown');
+  // $('.table-box').addClass('animated fadeInDown');
+  // $('#summerSample').addClass('animated fadeInDown');
 
-  setTimeout(function(){ 
-    $('.container').addClass('animated fadeOutDown');
-  }, 2000);
+  // setTimeout(function(){ 
+  //   $('.container').addClass('animated fadeOutDown');
+  // }, 2000);
 
-  $('.table-box').removeClass('hide');
-  $('#summerSample').removeClass('hide');    
+  // $('.table-box').removeClass('hide');
+  // $('#summerSample').removeClass('hide');    
   
-  setTimeout(function(){ 
-    $('#cssAnimate').trigger('click')
-  }, 2000);  
+  // setTimeout(function(){ 
+  //   $('#cssAnimate').trigger('click')
+  // }, 2000);  
 
 });
 
